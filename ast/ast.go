28710b2af63343d535187e3380e7e36455c391b4
Jayanth Manklu <jayanthms7@gmail.com>
2021-08-18 07:16:19 +0530

    add descriptive String() method to all types of nodes in AST for testing comprehension

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,7 @@
 package ast
 
 import "lookageek.com/ode/token"
+import "bytes"
 
 // AST structure holds the two basic types of code lines, statements & expressions
 // statements do not evaluate and present back a value, expressions do
@@ -8,6 +9,7 @@ import "lookageek.com/ode/token"
 // Node is the basic node of the AST which can hold a token
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement node holds a statement
@@ -37,6 +39,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
@@ -46,6 +58,10 @@ type Identifier struct {
 // places the identifier DOES produce a value like `let x = valueProducingIdentifier;`
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() string {
+	return i.Value
+}
 
 // The LetStatement should hold three values
 // the variable name, the token of the let statement
@@ -61,6 +77,21 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
 // ReturnStatement consists of only the "return" token
 // and the expression of value that needs to be returned
 type ReturnStatement struct {
@@ -72,4 +103,35 @@ func (rs *ReturnStatement) statementNode() {}
 
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
 }
