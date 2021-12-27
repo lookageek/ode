5cd0ac8041b162c3101f095e2d827cb389f1af10
Jayanth Manklu <jayanthms7@gmail.com>
2021-12-27 19:55:01 +0530

    builtin function - len() completed

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -312,14 +312,18 @@ func evalExpressions(
 // applyFunction takes a function object, uses the environment hierarchy and evaluates
 // the block statements inside function body
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+
+	case *object.Builtin:
+		return fn.Fn(args...)
+
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 // extendFunctionEnv constructs blank env with all the arguments to the function
@@ -354,7 +358,6 @@ func evalStringInfixExpression(operator string, left, right object.Object) objec
 	rightVal := right.(*object.String).Value
 	return &object.String{Value: leftVal + rightVal}
 }
-
 
 // newError is a constructor for the error object raised during evaluation
 func newError(format string, a ...interface{}) *object.Error {
