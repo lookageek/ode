0cbf5e42c1cf8a58e4f0238c192ce3dbe7a3ce38
Jayanth Manklu <jayanthms7@gmail.com>
2022-01-08 13:23:56 +0530

    add built-in function of puts

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "lookageek.com/ode/object"
+import (
+	"fmt"
+	"lookageek.com/ode/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -101,6 +104,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
