6abf85826a42f800699ceb7965142c0f77f881ed
Jayanth Manklu <jayanthms7@gmail.com>
2022-03-18 16:58:07 +0530

    first binary instruction generation complete with "OpConstant" opcode

--- code/code.go
+++ code/code.go
@@ -1,6 +1,9 @@
 package code
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 type Instructions []byte
 
@@ -10,6 +13,8 @@ const (
 	OpConstant Opcode = iota
 )
 
+// Definition is a handy debugging view of the opcode and
+// the number of operands an opcode will take
 type Definition struct {
 	Name          string
 	OperandWidths []int
@@ -26,4 +31,36 @@ func Lookup(op byte) (*Definition, error) {
 	}
 
 	return def, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	// increment the instruction length to total number of
+	// bytes all the operands will occupy for that opcode
+	// we need the total instructionLen to construct the requisite sized
+	// byte array for the instruction
+	instructionLen := 1
+	for _, operandWidth := range def.OperandWidths {
+		instructionLen += operandWidth
+	}
+
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, operand := range operands {
+		operandWidth := def.OperandWidths[i]
+		switch operandWidth {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(operand))
+		}
+
+		offset += operandWidth
+	}
+
+	return instruction
 }
